1e43d0d71d22d34c199c831c8b6fc550467a00c3
wrtirado <wrtirado@gmail.com>
2016-03-16 17:35:02 -0600

    BREAKTHROUGH Order and Menu are NG Repeating and populating

--- app.js
+++ app.js
@@ -21,28 +21,21 @@
     }
 
     // DrinkItem constructor
-    function DrinkItem(name, description, price, ingredients) {
+    function DrinkItem(name, description, price, ingredients, photo) {
       this.name = name;
       this.description = description;
       this.price = price;
       this.ingredients = ingredients;
+      this.photo = photo;
     }
 
     // Plate constructor
-    function Plate(name, description, price, ingredients) {
+    function Plate(name, description, price, ingredients, photo) {
       this.name = name;
       this.description = description;
       this.price = price;
       this.ingredients = ingredients;
-      this.isVegan = function(){
-        if(this.ingredient.fooditem.vegan === true){
-          this.isVegan = true
-        }
-        else {
-          this.isVegan = false
-        }
-
-      }
+      this.photo = photo
     }
 
     // Order constructor
@@ -67,110 +60,163 @@
       this.name = name;
       this.dietaryPreference = dietaryPreference;
     }
+
+    //  Function to push a plate onto the order array when button is clicked
+    RC.addPlate = function(plate){
+      RC.No1.orderArray.push(plate)
+    }
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     // All prototype functions that turn the objects built by our constructors into a coherent string
     //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
     FoodItem.prototype.stringify = function() {
-      console.log(this.name + ', contains ' + this.calories + ' calories , vegan =' + this.vegan + ', gluten free =' + this.glutenFree + ', citrus free = ' + this.citrusFree)
-    }
+      return this.name + ', contains ' + this.calories + ' calories , vegan =' + this.vegan + ', gluten free =' + this.glutenFree + ', citrus free = ' + this.citrusFree
+      }
 
 
     //-------------------------------------------------------------------
 
     DrinkItem.prototype.stringify = function() {
-        console.log(this.name + ' ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.')
+        return this.name + ' ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.'
       }
       // console.log(coke.stringify());
 
     //-------------------------------------------------------------------
 
     Plate.prototype.stringify = function() {
-        console.log(this.name + ': ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.')
+        return this.name + ': ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.'
       }
       // console.log(lunchSpecial.stringify());
 
     //-------------------------------------------------------------------
 
     Order.prototype.stringify = function() {
-        console.log('Your order contains ' + this.orderArray.join(', ') + '.')
+        return 'Your order contains ' + this.orderArray.join(', ') + '.'
       }
       // console.log(No1.stringify());
 
     //-------------------------------------------------------------------
 
     Plate.prototype.stringify = function() {
-        console.log(this.name + ': ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.')
+        return this.name + ': ' + this.description + ', costs $' + this.price + ', and contains ' + this.ingredients.join(', ') + '.'
       }
       // console.log(lunchSpecial.stringify());
 
     //-------------------------------------------------------------------
 
     Restaurant.prototype.stringify = function() {
-        console.log('Welcome to ' + this.name + '!' + this.description + ' Please, peruse our menu: ' + this.menu + '.')
+        return 'Welcome to ' + this.name + '!' + this.description + ' Please, peruse our menu: ' + this.menu + '.'
       }
       // console.log(newRestaurant.stringify());
 
+    //-------------------------------------------------------------------
+
+
     Menu.prototype.stringify = function() {
+      var stringifiedMenu = ''
       this.menuArray.forEach(function(element) {
-        console.log(element.name);
+        stringifiedMenu += element.name + ', '
       })
+      return stringifiedMenu.slice(0, -2)
     }
 
+   //-------------------------------------------------------------------
     Order.prototype.stringify = function() {
+      var stringifiedOrder = ''
         this.orderArray.forEach(function(element) {
-          console.log(element.name);
+          stringifiedOrder += element.name + ', '
         })
+        return stringifiedOrder
       }
-      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-      // Our three food items, built by our FoodItem constructor
-      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
 
-    var bacon = new FoodItem('bacon', 250, false, true, false)
-    console.log(bacon)
-    bacon.stringify()
+   //-------------------------------------------------------------------
 
-    var hotPocket = new FoodItem('hot pocket', 1000, false, false, false)
-    hotPocket.stringify()
-
-    var potato = new FoodItem('potato', 100, true, true, true)
-    potato.stringify()
-
-    var water = new FoodItem('water', 0, true, true, true)
-    var highFructoseCornSyrup = new FoodItem('High Fructose Corn Syrup', 100, true, true, true)
-
-    var coke = new DrinkItem('coke', 'rots your teeth', 1, [water, highFructoseCornSyrup])
-    console.log(coke);
-
-    var lunchSpecial = new Plate('Glutenful Lunch Special', 'Bread, bread and more bread', 19, ['bread', 'croissont', 'foccacia'])
-    console.log(lunchSpecial);
-
-    var No1 = new Order([lunchSpecial, lunchSpecial])
-    console.log(No1);
-
-    var No2 = new Order(lunchSpecial)
-
-    var newRestaurant = new Restaurant("Bo's Diner ", "We are a family owned restaurant, owned and operated since 1985!", lunchSpecial)
-
-    var jim = new Customer('Jim', true)
-
-    var boMenu = new Menu([lunchSpecial, lunchSpecial])
-    boMenu.stringify()
-
-    var willOrder = new Order([lunchSpecial, lunchSpecial])
-    willOrder.stringify()
-      // Define a stringify method on each constructor's prototype. This method should return a string representation of the object, formatted as you choose. stringify should not have any side effects. You may want to reuse the stringify method of the contained objects. (e.g. the Menu object should call stringify on each of its plates to reuse that functionality instead of having to duplicate the code for each plate).
 
       //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
       // Add the following methods to the Plate object to determine if all the food items within it fit a specific dietary restriction.
       //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
-      //
       // boolean isVegan
+      Plate.prototype.isVegan = function(){
+        for(var i = 0; i < this.ingredients.length; i++){
+        return this.ingredients[i].vegan
+        }
+      }
+
       // boolean isGlutenFree
+      Plate.prototype.isGlutenFree = function(){
+        for(var i = 0; i < this.ingredients.length; i++){
+        return this.ingredients[i].glutenFree
+        }
+      }
       // boolean isCitrusFree
-      // Instantiate a Burrito Plate, a Guacamole Plate, and a Margarita Drink.
-      // Instantiate a Menu that contains each of the instantiated Plates and Drinks.
-      // Instantiate a Restaurant that contains the instantiated Menu.
+      Plate.prototype.isCitrusFree = function(){
+        for(var i = 0; i < this.ingredients.length; i++){
+        return this.ingredients[i].citrusFree
+        }
+      }
+
+
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+      // Our three food items, built by our FoodItem constructor
+      //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+    RC.bacon = new FoodItem('bacon', 250, false, true, false)
+
+
+
+    RC.hotPocket = new FoodItem('hot pocket', 1000, false, false, false)
+
+
+    RC.potato = new FoodItem('potato', 100, true, true, true)
+
+
+    RC.water = new FoodItem('water', 0, true, true, true)
+    RC.highFructoseCornSyrup = new FoodItem('High Fructose Corn Syrup', 100, true, true, true)
+
+    RC.coke = new DrinkItem('coke', 'rots your teeth', 1, [RC.water, RC.highFructoseCornSyrup])
+
+
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // Instantiate a Burrito Plate, a Guacamole Plate, and a Margarita Drink.
+    // Instantiate a Menu that contains each of the instantiated Plates and Drinks.
+    // Instantiate a Restaurant that contains the instantiated Menu.
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    RC.burritoPlate = new Plate('Bo\'s Burrito', 'A gut bomb full of guac', 12, [RC.bacon, RC.potato, RC.hotPocket], "http://2.bp.blogspot.com/-HQzNiJWGIGM/Ug4r9Qi5GMI/AAAAAAAAY20/lu0_4XzJphs/s1600/enchirito.jpg")
+
+    RC.guacamolePlate = new Plate('Guillermo\'s Guac', 'The great green guac to put in the gut bomb', 10, [RC.bacon, RC.potato, RC.hotPocket], "http://www.puregoodness.net/wp-content/uploads/2011/02/guac.jpg")
+
+    RC.margaritaDrink = new DrinkItem('Mamma\'s Margarita', 'Be warned, Mamma likes her booze', 9, [RC.bacon, RC.potato, RC.hotPocket, RC.coke], "http://s3-media3.fl.yelpcdn.com/bphoto/vpB04tTNpxq-ZLQzdGt5tg/348s.jpg")
+console.log(RC.margaritaDrink.photo);
+
+    RC.lunchSpecial = new Plate('Glutenful Special', 'Bread, bread and more bread', 19, [RC.bacon, RC.potato, RC.hotPocket], "http://graphics8.nytimes.com/images/2015/10/28/health/well_moldy/well_moldy-tmagArticle.jpg")
+
+    RC.lunchSpecial.isVegan()
+    RC.lunchSpecial.isGlutenFree()
+    RC.lunchSpecial.isCitrusFree()
+
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // Creating our orders and menu that contain the plates
+    //-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+    // var restMenu = boMenu.stringify()
+
+    RC.No1 = new Order([])
+    console.log(RC.No1.orderArray);
+
+    RC.jim = new Customer('Jim', true)
+
+    RC.boMenu = new Menu([RC.lunchSpecial, RC.burritoPlate, RC.guacamolePlate, RC.margaritaDrink])
+
+    RC.newRestaurant = new Restaurant("Bo's Diner", " We are a family owned restaurant, owned and operated since 1985!", RC.boMenu.stringify())
+
+    RC.willOrder = new Order([RC.lunchSpecial, RC.margaritaDrink])
+    RC.willOrder.stringify()
+      // Define a stringify method on each constructor's prototype. This method should return a string representation of the object, formatted as you choose. stringify should not have any side effects. You may want to reuse the stringify method of the contained objects. (e.g. the Menu object should call stringify on each of its plates to reuse that functionality instead of having to duplicate the code for each plate).
+
+
       // Call the Restaurant's stringify method to have it print out all its information.
+console.log(RC.newRestaurant.stringify())
+
+
+
   }
 })();
